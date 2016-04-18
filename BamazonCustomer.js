4896ef9f0f63ab74b897b0ef711278b28c68bd6a
m. nathaniel martin <matt.martin@rutgers.edu>
2016-04-18 13:45:32 -0400

    reorder prompt and such

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -20,31 +20,38 @@ var schema = {
   }
 };
 
-prompt.start();
 
-prompt.get(schema, function (err, result) {
-  //
-  // Log the results.
-  //replace these with mySQL queries from wseparate module. RIght now it only logs the number you type.
-  console.log('Command-line input received:');
-  console.log('  You want Item #: ' + result.ItemID);
 
-});
+
+
+
+
+// GOTTADO: prompt for an order query.
+//if input is INT, query by ItemID. If a VARCHAR, query by Name. **aDD qUERY TO A qUERYtABLE.
+
+//SEARCH BY QUERY,
 
 connection.connect();
 
 connection.query(queries.showInventory, function(err, rows, fields) {
   if (err) throw err;
 
-  console.log("Welcome to the Bamazon Storefront".bold.cyan);
+  console.log("Welcome to the Bamazon Storefront!".bold.cyan);
   for(var i=0;i<rows.length;i++){
     console.log(("item " + rows[i].ItemID).black.bgCyan + " " + rows[i].Name.bold + " || $" + rows[i].Price.toFixed(2) + (" (" + rows[i].StockQuantity + " available)").grey);
   }
+
+  prompt.start();
+
+  prompt.get(schema, function (err, result) {
+    //
+    // Log the results.
+    //replace these with mySQL queries from wseparate module. RIght now it only logs the number you type.
+    console.log('Command-line input received:');
+    console.log('  You want Item #: ' + result.ItemID);
+
+  });
+
 });
-
-// GOTTADO: prompt for an order query.
-//if input is INT, query by ItemID. If a VARCHAR, query by Name. **aDD qUERY TO A qUERYtABLE.
-
-//SEARCH BY QUERY,
 
 connection.end();
