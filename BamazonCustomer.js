a1fc044339b0e95a1f91987b883389b4b700c660
m. nathaniel martin <matt.martin@rutgers.edu>
2016-04-18 02:18:42 -0400

    prompting in progress -- need to call the mysql connect WITIHIN the prompt, for that I will create a module.

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,6 +1,7 @@
 var mysql      = require('mysql');
 var colors = require('colors');
 var queries = require('./queries');
+var prompt = require('prompt');
 
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -9,18 +10,41 @@ var connection = mysql.createConnection({
   database : 'Bamazon'
 });
 
+var schema = {
+  properties: {
+    ItemID: {
+      description: 'Enter an ItemID to get started!',
+      message: 'error. try again.',
+      required: true
+    }
+  }
+};
+
+prompt.start();
+
+prompt.get(schema, function (err, result) {
+  //
+  // Log the results.
+  //
+  console.log('Command-line input received:');
+  console.log('  You want Item #: ' + result.ItemID);
+
+});
+
 connection.connect();
 
 connection.query(queries.showInventory, function(err, rows, fields) {
   if (err) throw err;
 
-  // query the database, return all items for sale.
   console.log("Bamazon Storefront".bold.cyan);
   for(var i=0;i<rows.length;i++){
     console.log(("item " + rows[i].ItemID).black.bgCyan + " " + rows[i].Name.bold + " || $" + rows[i].Price.toFixed(2) + (" (" + rows[i].StockQuantity + " available)").grey);
   }
 });
 
-// prompt for an order query.
+// GOTTADO: prompt for an order query.
+//if input is INT, query by ItemID. If a VARCHAR, query by Name. **aDD qUERY TO A qUERYtABLE.
+
+//SEARCH BY QUERY,
 
 connection.end();
